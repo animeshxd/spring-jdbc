b98571dd193905f2f99eaca617bd94c7c1cdab2e
Animesh Murmu <am2646374@gmail.com>
2022-08-04 14:42:21 +0530

    JdbcTemplate bean with sqlite configuration

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -1,9 +1,13 @@
 package example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var ctx = new ClassPathXmlApplicationContext("spring.xml");
+        var jdbc = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
+
+        ctx.close();
     }
 }
